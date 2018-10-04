b9abe6e7bfe7d7f6c00075097a1aec61c75bd53b
DaanFrank <a3228644@gmail.com>
2018-10-04 22:45:52 +0800

    test

--- app/src/main/java/nkust/schoolpay/MainActivity.java
+++ app/src/main/java/nkust/schoolpay/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    int frank = 126345 ;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
